1ba492a26738f25e9a57f3e760ea87d003de0198
huangwei <huangwei@aiitec.org>
2018-09-26 18:37:45 +0800

    first commit

---
+++ ChatToolBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatToolBar
+//
+//  Created by 快摇002 on 2018/9/26.
+//  Copyright © 2018年 aiitec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatToolBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatToolBar
+//
+//  Created by 快摇002 on 2018/9/26.
+//  Copyright © 2018年 aiitec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
